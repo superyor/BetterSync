ba58a8d6b504dd4ed062ce9f7010c9057ac32f0f
Superyu <49453028+superyu1337@users.noreply.github.com>
2021-04-05 23:38:32 +0200

    Cleared repo
    bye bye. old ragesu :heart:

--- RageSU.lua
+++
@@ -1,5 +0,0 @@
-local ragesu = http.Get("https://raw.githubusercontent.com/superyor/RageSU/master/RageSU%20Downloader.lua");
-local f = file.Open("Data\\Superyu\\RageSU\\Temp\\scriptloader.lua", "w");
-f:Write(ragesu);
-f:Close();
-LoadScript("Data\\Superyu\\RageSU\\Temp\\scriptloader.lua")
